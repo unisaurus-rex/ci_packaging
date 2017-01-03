0ed58af26050c03a84ba56c4d90d99317b8f1a2e
nicole-cranon <nicole.cranon@gmail.com>
2017-01-03 14:35:30 -0700

    added a build directory for build specific tasks, added comments, changed parseCsvs arg, added call to  build.putJsonJsFilesProjectSrcDir()

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var dirCheck = directoryManip.dirCheck;
 var lib = require("./lib");
 var messages = lib.messages;
 
+var build = require("./build");
+
 // move to appropriate place and adjust paths accordingly
 //zipDir("../example-front-end-shell-app","../example-front-end-shell-app.7z");
 
@@ -19,12 +21,12 @@ var messages = lib.messages;
 var csvDir = process.argv[2]; // Directory containing the .csv files
 var projectDir = process.argv[3]; // Directory containing front-end project, html, js, css files
 
-if (typeof csvDir === "undefined") {
+if (typeof csvDir === "undefined") {  // no argument was passed for csvDir/argv[2]
   messages.usage();
   process.exit(1);
 }
 
-if (typeof projectDir === "undefined") {
+if (typeof projectDir === "undefined") { // no argument was passed for projectDir/argv[3]
   messages.usage();
   process.exit(1);
 }
@@ -35,11 +37,15 @@ async.series([
   dirCheck.exists(projectDir),
   dirCheck.isDirectory(csvDir),
   dirCheck.isDirectory(projectDir),
-  parseCsvs("../example-front-end-shell-app/src"),
+
+  parseCsvs("../example-front-end-shell-app/resources/csvs"),  // parse each csv file in given directory to a .js file
+
+  build.putJsonJsFilesProjectSrcDir("../example-front-end-shell-app/resources/csvs", "../example-front-end-shell-app/src"),   // place each .js file into the project/src folder
+
 
 ], function (err, results) {
   if(err){
-    console.error("An error occurred." + err);
+    console.error("An error occurred. " + err);
     process.exit(1);
   }
 
