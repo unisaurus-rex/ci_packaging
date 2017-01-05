243a6e4bca0fd68779e7a1fcb81fa413bd8ff8ce
nicole-cranon <nicole.cranon@gmail.com>
2017-01-05 08:21:16 -0700

    removed csv, using csv-parse directly, had to make parseCsv synchronous, next functions would not wait for .js files to be written, added comments

--- directory_manipulation/parse-csvs-to-json.js
+++ directory_manipulation/parse-csvs-to-json.js
@@ -1,24 +1,29 @@
 "use strict";
 
-var csv = require("csv");
+//var csv = require('csv');
+var parse = require('csv-parse/lib/sync');
 var fse = require("fs-extra");
 var _ = require("lodash");
+var async = require("async");
 
 /**
+ * parseCsvs
  * Used to parse all .csv files in the passed dir into .json files, such that there will be one .json file for each .csv
  * @param dir director to read in .csv files
- * @author Jeff Flower
  */
 module.exports = function parseCsvs(dir) {
   return function parseCsvsCb(callback) {
-    var files = fse.readdirSync(dir);
+    var files = fse.readdirSync(dir); // get all files in the provided directory, sans . and ..
 
+    // filter to only those with .csv extension
     files = _.filter(files, function (file) {
       return file.toString().endsWith(".csv");
     });
 
-    //TODO - change to map or would you????
+    // iterate through each .csv file and parse to json and write to file with .js extension
     _.forEach(files, function (file) {
+      //parseCsv(dir, file, callback);
+
       parseCsv(dir, file);
     });
 
@@ -29,51 +34,128 @@ module.exports = function parseCsvs(dir) {
 };
 
 function parseCsv(dir, file) {
+  var input = fse.readFileSync(dir.concat("/", file), {
+    encoding: "utf8"
+  });
 
-  //var file = __dirname + "/" + process.argv[2]; // csv file to parse should be argument when running program
-  var parser = csv.parse({columns: true}); // use first row as headerss
+  //console.log(input);
+
+
+  var contents = parse(input, {
+    columns: true
+  });
+
+  //console.log(contents);
 
   var output = {};
-
-  parser.on('readable', function readableCb () {
-    var record;
-    while (record = parser.read()) {
-      addToOutput(record);
-    }
+  _.forEach(contents, function (record) {
+    addToOutput(record, output);
   });
 
-  // write new .js file to hold the originally .csv content parsed into .json format
-  parser.on('finish', function finishCb () {
-    var data = "export const dataJSON = \'" + JSON.stringify(output) + "\';";
+  //console.log(output);
 
-    fse.writeFileSync(dir + "/" + file + ".js", data);
-  });
+  var data = "export const dataJSON = \'" + JSON.stringify(output) + "\';";
 
-  fse.createReadStream(dir + "/" + file).pipe(parser);
+  fse.writeFileSync(dir + "/" + file + ".js", data);
+}
 
-  // parse a record into the correct place in output
-  // a record will be an object with keys
-  function addToOutput(obj) {
-    var txn = obj.txn_type;
-    var fi = obj.fi;
+/**
+ * parseCsv
+ *
+ * Used to parse a .csv file into a .json file
+ *
+ * @param dir directory to read in .csv files
+ * @param file should be .csv file
+ * @author Jeff Flower
+ */
+//function parseCsv(dir, file, callback) {
+//  //var file = __dirname + "/" + process.argv[2]; // csv file to parse should be argument when running program
+//  var parser = csv.parse({columns: true}); // use first row as headerss
+//
+//  var output = {};
+//
+//  // what to do on readable content read content of the csv file and append to json output
+//  parser.on('readable', function readableCb() {
+//    var record;
+//    while (record = parser.read()) {  // so long as there is content continue reading and parsing
+//
+//      addToOutput(record);
+//    }
+//  });
+//
+//  // what to do on completion of reading, write new .js file to hold the originally .csv content parsed into .json format
+//  parser.on('finish', function finishCb() {
+//    var data = "export const dataJSON = \'" + JSON.stringify(output) + "\';";
+//
+//    try {
+//      fse.writeFileSync(dir + "/" + file + ".js", data);
+//
+//      console.log("Parsed " + dir + "/" + file + " to " + dir + "/" + file + ".js");
+//    } catch (e) {
+//      return callback("Error writing " + dir + "/" + file + ".js. " + e);
+//    }
+//
+//    //fse.writeFile(dir + "/" + file + ".js", data, function (err) {
+//    //  if (err) {
+//    //    return callback("Error writing " + dir + "/" + file + ".js. " + e);
+//    //  }
+//    //
+//    //  console.log("Wrote " + dir + "/" + file + ".js");
+//    //});
+//
+//  });
+//
+//  // begin reading .csv file, pipe to parser
+//  fse.createReadStream(dir + "/" + file).pipe(parser);
+//
+//  // parse a record into the correct place in output
+//  // a record will be an object with keys
+//  function addToOutput(obj) {
+//    var txn = obj.txn_type;
+//    var fi = obj.fi;
+//
+//    var rest = obj;
+//    delete rest.txn_type;
+//    delete rest.fi;
+//
+//    // check if output already has a key matching txn
+//    if (output.hasOwnProperty(txn)) {
+//      // checkout if output[txn] already has a key matching fi
+//      if (output[txn].hasOwnProperty(fi)) {
+//        output[txn][fi].push(rest);
+//      } else {
+//        // fi key doesn't exist, add it and set it's value to rest
+//        output[txn][fi] = [rest];
+//      }
+//    } else {
+//      // txn key doesn't exist
+//      output[txn] = {};
+//      output[txn][fi] = [rest];
+//    }
+//  }
+//}
 
-    var rest = obj;
-    delete rest.txn_type;
-    delete rest.fi;
 
-    // check if output already has a key matching txn
-    if (output.hasOwnProperty(txn)) {
-      // checkout if output[txn] already has a key matching fi
-      if (output[txn].hasOwnProperty(fi)) {
-        output[txn][fi].push(rest);
-      } else {
-        // fi key doesn't exist, add it and set it's value to rest
-        output[txn][fi] = [rest];
-      }
+function addToOutput(obj, output) {
+  var txn = obj.txn_type;
+  var fi = obj.fi;
+
+  var rest = obj;
+  delete rest.txn_type;
+  delete rest.fi;
+
+  // check if output already has a key matching txn
+  if (output.hasOwnProperty(txn)) {
+    // checkout if output[txn] already has a key matching fi
+    if (output[txn].hasOwnProperty(fi)) {
+      output[txn][fi].push(rest);
     } else {
-      // txn key doesn't exist
-      output[txn] = {};
+      // fi key doesn't exist, add it and set it's value to rest
       output[txn][fi] = [rest];
     }
+  } else {
+    // txn key doesn't exist
+    output[txn] = {};
+    output[txn][fi] = [rest];
   }
 }
