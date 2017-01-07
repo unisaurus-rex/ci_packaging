c931ce1839be10cca0a79e6f5a7dea1ebacb477f
nicole-cranon <nicole.cranon@gmail.com>
2017-01-06 17:51:26 -0700

    added command line arguments for target directory and target name, made command line args const, removed some hardcoded values

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ var messages = lib.messages;
 var build = require("./lib/build");
 
 // csv and project directory comes in as follows "node app.js <csv-dir> <project-dir>"
-var csvDir = process.argv[2]; // Directory containing the .csv files
-var projectDir = process.argv[3]; // Directory containing front-end project, html, js, css files
+const csvDir = process.argv[2]; // Directory containing the .csv files
+const projectDir = process.argv[3]; // Directory containing front-end project, html, js, css files
+const targetDir = process.argv[4];  // Directory to output zip files
+// TODO should probably be getting name of zip from fi name, but need to first get fi name
+const targetName = process.argv[5]; // Name of output zip file
 
 if (typeof csvDir === "undefined") {  // no argument was passed for csvDir/argv[2]
   messages.usage();
@@ -36,26 +39,22 @@ async.series([
   dirCheck.isDirectory(csvDir),
   dirCheck.isDirectory(projectDir),
 
-  // TODO remove these hardcoded values
-  parseCsvs("../csvs"),  // parse each csv file in given directory to a .js file
+  // parse each csv file in given directory to a .js file
+  parseCsvs(csvDir),
 
   // TODO remove these hardcoded values
-  build.putJsonJsFileProjectSrcDir("../csvs/ci_demo.csv.js", "../ci-interim-develop/src"),   // place each .js file into the project/src folder
+  build.putJsonJsFileProjectSrcDir(csvDir.concat("/ci_demo.csv.js"), projectDir.concat("/src/scripts")),   // place each .js file into the project/src folder
 
   // make build folder & copy index.html to build folder
 
-  // call npm build funcs
-  build.run("../ci-interim-develop/src"),  // currently cannot install jspm globally
+  // call npm build scripts
+  build.run(projectDir.concat("/src")),
 
-  // zip & store & remove build folder
-  //zipDir("../ci-interim-develop/build", "C:/Users/U999716/Desktop/ci-interim.zip")
-  build.pkg("C:/Users/U999716/Desktop/ci-interim.zip", "../ci-interim-develop/build")
-], function appJsCb (err, results) {  // if any of the previous functions fails should end up in the following cb with err
+  // zip & store
+  build.pkg(targetDir, targetName, projectDir.concat("/build"))
+], function appJsCb(err, results) {  // if any of the previous functions fails should end up in the following cb with err
   if (err) {
     console.error("An error occurred. " + err);
     process.exit(1);
   }
 });
-
-//console.log("\nAll done!");
-
